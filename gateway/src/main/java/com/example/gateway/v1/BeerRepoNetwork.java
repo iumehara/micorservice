ae5b655c3924b016cf4c54e46f8b3b3a7a1e6856
iumehara <iumehara10@gmail.com>
2020-06-15 08:25:36 +0900

    use @Async annotation

--- gateway/src/main/java/com/example/gateway/v1/BeerRepoNetwork.java
+++ gateway/src/main/java/com/example/gateway/v1/BeerRepoNetwork.java
@@ -1,31 +1,27 @@
 package com.example.gateway.v1;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @Repository
 public class BeerRepoNetwork implements BeerRepo {
 
+    @Async
     @Override
     public Future<List<Beer>> getAll() {
         RestTemplate restTemplate = new RestTemplate();
-        CompletableFuture<List<Beer>> beersFuture = new CompletableFuture<>();
-        Executors.newCachedThreadPool().submit(() -> {
-            String beerServiceUrl = "http://localhost:8081";
-            ResponseEntity<Beer[]> response = restTemplate.getForEntity(beerServiceUrl, Beer[].class);
-            List<Beer> beers = asList(requireNonNull(response.getBody()));
-            beersFuture.complete(beers);
-        });
-
-        return beersFuture;
+        String beerServiceUrl = "http://localhost:8081";
+        ResponseEntity<Beer[]> response = restTemplate.getForEntity(beerServiceUrl, Beer[].class);
+        List<Beer> beers = asList(requireNonNull(response.getBody()));
+        return completedFuture(beers);
     }
 }
